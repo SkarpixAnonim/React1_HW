4ba8ee470176dac03ca6485d2179b13a12f9c3e4
SkarpiX <supradarkfull@gmail.com>
2023-08-24 17:01:39 +0300

    complate hw

---
+++ dz2_emir_22-01/src/Time.js
@@ -0,0 +1,24 @@
+import React, {useEffect, useState} from "react";
+
+function Time() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const hours = currentTime.getHours().toString().padStart(2, '0');
+  const minutes = currentTime.getMinutes().toString().padStart(2, '0');
+  const seconds = currentTime.getSeconds().toString().padStart(2, '0');
+
+  return (
+    <div>
+      <p>{hours}:{minutes}:{seconds}</p>
+    </div>
+  );
+}
+
+export default Time;
---
+++ dz2_emir_22-01/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import Time from "./Time";
+import PhotoGallery from "./Time";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Time/>
+  </React.StrictMode>
+);
+
